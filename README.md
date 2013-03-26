3588aafbb7822f76aecc7ad0bc526817ebe9a104
lzqwebsoft <751939573@qq.com>
2013-03-26 17:20:37 +0800

    Preliminary implemented crawl the information of weibo .

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
-test-project
-============
+#test-project
 
 用于练习github使用
+
+##测试抓取微博信息
+
+####Tencent API
+
+<font color="red">每个操作url，都要使用 Cookie</font>
+
+账号检测：
+检查账号是否要验证码
+http://check.ptlogin2.qq.com/check?uin=:account&appid=1003903&r=" + Math.random()
+参数：
+	uin 账号
+	appid 腾讯应用id，固定
+	r 随机数，防缓存
+返回值：
+	ptui_checkVC('0','!RT1','\x00\x00\x00\x00\x00\x5e\xbb\x3d');
+	第一个： 0 需要验证码， 1 不需要验证码
+	第二个：如果 ！ 开头，直接为明文验证码用于登录加密密码，否则获取验证码
+	第三个：登录密码加密串
+
+-----------------------------
+
+验证码：
+http://captcha.qq.com/getimage?aid=1003903&r=" + Math.random() + "&uin=" + account
+参数：
+	uin 账号
+返回值：
+	InputStream,图片流
+
+注：这个验证码将会代替为 账号检测第二个参数
+
+-----------------------------
+
+登录：
+http://ptlogin2.qq.com/login?u=:account&p=:password&verifycode=:VCode&webqq_type=10&remember_uin=1&login2qq=1&aid=1003903&u1=:loginurl&h=1&ptredirect=0&ptlang=2052&from_ui=1&pttype=1&dumy=&fp=loginerroralert&action=7-24-1937704&mibao_css=m_webqq&t=1&g=1
+参数：
+	u 登录账号
+	p 加密后的密码(参考 encodePass.js[passwordEncoding(password, \x00第三个, 验证码)])
+	verifycode 验证码
+	u1 登录产品的url, http://web.qq.com/loginproxy.html?login2qq=1&webqq_type=10
+返回值：
+	ptuiCB('0','0','http://web3.qq.com/loginproxy.html?login2qq=1&webqq_type=40','0','登录成功！', '承∮诺');
+	第一个参数的意义:
+		0：登录成功!
+		1：系统繁忙，请稍后重试。
+		2：已经过期的QQ号码。
+		3：您输入的密码有误，请重试。
+		4：您输入的验证码有误，请重试。es
+		5：校验失败。
+		6：密码错误。如果您刚修改过密码, 请稍后再登录.
+		7：您的输入有误, 请重试。
+		8：您的IP输入错误的次数过多，请稍后再试。
+
+-----------------------------
+
+获取微博信息
+第一页使用：
+http://api1.t.qq.com/asyn/home.php?&&apiType=8&apiHost=http://api.t.qq.com&_r=:_r1&_r=:_r2
+参数：
+	第一个_r1: 时间截 ， 不知是什么，猜想为当前时间的时间截
+	第二个_r2: 时间截，猜想为当前时间的时间截加上6263
+返回值：
+	JSON形式的微博数据
+
+-----------------------------
+
+获取微博信息
+下一页使用：
+http://api1.t.qq.com/asyn/home.php?&time=:time&page=:page&id=:id&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=:r
+参数：
+	time 上一次请求返回的最后一条记录时间，对应JSON数据中的timestamp
+	page 下页的页数
+	id 上一次请求返回的最后一条记录id，对应JSON数据中的id
+	r 时间截 ， 猜想为当前时间的时间截
+返回值：
+	JSON形式的微博数据
+
+<font color="red">注意通过http://api1.t.qq.com/asyn/home.php抓取微博信息时，需要设置请求头Referer与User-Agent的信息。</font>
--- src/lzwebsoft/app/weibo/test/LoginTest.java
+++ src/lzwebsoft/app/weibo/test/LoginTest.java
@@ -1,16 +1,24 @@
 package lzwebsoft.app.weibo.test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import lzwebsoft.app.weibo.util.Encryptor;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -22,25 +30,263 @@ import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.jsoup.helper.StringUtil;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
- * 测试登录
+ * 测试抓取微博信息
  * @author zqluo
  *
  */
 public class LoginTest {
 	public static final String CHECK_VERIFY_URL = "http://check.ptlogin2.qq.com/check?uin={0}&appid=1003903&r={1}";
 	public static final String USER_LOGIN_URL = "http://ptlogin2.qq.com/login";
-	public static final String CHANNEL_LOGIN_URL = "http://d.web2.qq.com/channel/login2";
+	public static final String VALIDATION_CODE_URL = "http://captcha.qq.com/getimage?aid=1003903&r={0}&uin={1}";
 	
-	public static final String WEIBO_URL = "http://t.qq.com/";
-	public static final String MORE_INFO_URL = "http://api1.t.qq.com/asyn/home.php";
+	public static final String WEIBO_JSON_URL = "http://api1.t.qq.com/asyn/home.php";
 	
 	public static final String REGXP_CHECK_VERIFY = "ptui_checkVC\\('(.*?)','(.*?)','(.*?)'\\)";
+	public static final String REGXP_CHECK_LOGIN = "ptuiCB\\('(\\d+)','(\\d+)','(.*?)','(\\d+)','(.*?)', '(.*?)'\\)";
+	
+	// 获取验证码
+	public static JFrame validationCode(HttpClient httpClient, HttpGet httpGet, String account) {
+		String url = MessageFormat.format(VALIDATION_CODE_URL, Math.random(), account);
+		httpGet = new HttpGet(url);
+		try {
+			// 从远程服务器上得到ImageIcon对象
+			HttpResponse reponse = httpClient.execute(httpGet);
+			HttpEntity entery = reponse.getEntity();
+			InputStream stream = entery.getContent();
+			ImageIcon image = new ImageIcon(ImageIO.read(stream));
+			
+			// 使用窗体显示图片
+			JFrame frame = new JFrame();
+			frame.setTitle("验证码");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setLocationRelativeTo(null); // 居中对齐
+			
+			JLabel verifyLabel = new JLabel(image);
+			verifyLabel.setSize(image.getIconWidth(), image.getIconHeight());
+			verifyLabel.setOpaque(true);
+			verifyLabel.setBorder(null);
+			frame.add(verifyLabel);
+			frame.setSize(200, image.getIconHeight()+50);
+			frame.setResizable(false);
+			frame.setVisible(true);
+			frame.setAlwaysOnTop(true);
+			
+			return frame;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// 登录并抓取微博信息
+	public static void login(HttpClient httpClient, HttpGet httpGet, long uin, String accountNumber, String accountPassword, String codeNum)
+	    throws ClientProtocolException, IOException {
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("u", accountNumber));
+		formparams.add(new BasicNameValuePair("p", Encryptor.encrypt(uin, accountPassword, codeNum)));
+		formparams.add(new BasicNameValuePair("verifycode", codeNum));
+		formparams.add(new BasicNameValuePair("webqq_type", "10"));
+		formparams.add(new BasicNameValuePair("remember_uin","1"));
+		formparams.add(new BasicNameValuePair("login2qq", "1"));
+		formparams.add(new BasicNameValuePair("aid", "1003903"));
+		formparams.add(new BasicNameValuePair("u1", "http://web.qq.com/loginproxy.html?login2qq=1&webqq_type=10"));
+		formparams.add(new BasicNameValuePair("h", "1"));
+		formparams.add(new BasicNameValuePair("ptredirect", "0"));
+		formparams.add(new BasicNameValuePair("ptlang", "2052"));
+		formparams.add(new BasicNameValuePair("from_ui", "1"));
+		formparams.add(new BasicNameValuePair("pttype", "1"));
+		formparams.add(new BasicNameValuePair("dumy", ""));
+		formparams.add(new BasicNameValuePair("fp", "loginerroralert"));
+		formparams.add(new BasicNameValuePair("action", "2-13-47578"));
+		formparams.add(new BasicNameValuePair("mibao_css", "m_webqq"));
+		formparams.add(new BasicNameValuePair("t", "1"));
+		formparams.add(new BasicNameValuePair("g", "1"));
+		
+		String prams = URLEncodedUtils.format(formparams, "UTF-8");
+		StringBuffer login_url = new StringBuffer(USER_LOGIN_URL);
+		login_url.append("?").append(prams);
+		httpGet = new HttpGet(login_url.toString());
+		httpGet.setHeader("Referer", "http://qzone.qq.com/");
+		HttpResponse response = httpClient.execute(httpGet);
+		CookieStore cookieStore=((AbstractHttpClient) httpClient).getCookieStore();
+		
+		String context = EntityUtils.toString(response.getEntity());
+		Pattern p = Pattern.compile(REGXP_CHECK_LOGIN);
+		Matcher m = p.matcher(context);
+		int resultCode = 1;
+		String msg = "";
+		String userName = "";
+		if(m.find()){
+			msg = m.group(5);
+        	resultCode = Integer.parseInt(m.group(1));
+        	userName = m.group(6);
+		}
+		
+		if(resultCode==0) {
+			// 登录成功
+			System.out.print(userName+"$");
+			System.out.println(msg);
+			
+			// 抓取第一页微博
+			//http://api1.t.qq.com/asyn/home.php?&&apiType=8&apiHost=http://api.t.qq.com&_r=1364277155700&_r=1364277161963
+			Date now = new Date();
+			long _r = now.getTime();
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("apiType", "8"));
+			params.add(new BasicNameValuePair("apiHost", "http://api.t.qq.com"));
+			params.add(new BasicNameValuePair("_r", String.valueOf(_r)));
+			params.add(new BasicNameValuePair("_r", String.valueOf(_r+6263)));
+			
+			String paramsStr = URLEncodedUtils.format(params, "UTF-8");
+			StringBuffer url = new StringBuffer(WEIBO_JSON_URL);
+			url.append("?&&").append(paramsStr);
+			
+			httpGet = new HttpGet(url.toString());
+			httpGet.setHeader("Referer", "http://api1.t.qq.com/proxy.html");
+			httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0");
+			DefaultHttpClient httpClient2 = new DefaultHttpClient();
+			httpClient2.setCookieStore(cookieStore);
+			response = httpClient2.execute(httpGet);
+			cookieStore = httpClient2.getCookieStore();
+			
+			context = EntityUtils.toString(response.getEntity());
+			
+			int currentPg = 1;  // 标记当前页数
+			String[] nextPgInfo = parserJSON(context);
+			
+			// 微博翻页
+			Scanner in = new Scanner(System.in);
+			while(nextPgInfo!=null&&nextPgInfo.length==2) {
+				
+				System.out.print("继续浏览下一页(Y/N)?: ");
+				String order = in.next();
+				if(order!=null&&order.trim().equalsIgnoreCase("Y")) {
+					currentPg++;
+					
+					List<NameValuePair> moreParams = new ArrayList<NameValuePair>();
+					moreParams.add(new BasicNameValuePair("time", nextPgInfo[0]));
+					moreParams.add(new BasicNameValuePair("page", String.valueOf(currentPg)));
+					moreParams.add(new BasicNameValuePair("id", nextPgInfo[1]));
+					moreParams.add(new BasicNameValuePair("apiType", "8"));
+					moreParams.add(new BasicNameValuePair("apiHost", "http://api.t.qq.com"));
+					moreParams.add(new BasicNameValuePair("_r", String.valueOf(new Date().getTime())));
+					prams = URLEncodedUtils.format(moreParams, "UTF-8");
+					
+					//http://api1.t.qq.com/asyn/home.php?&time=1364121424&page=9&id=235984052151296&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364191855494
+					StringBuffer more_info_url = new StringBuffer(WEIBO_JSON_URL);
+					more_info_url.append("?&").append(prams);
+					HttpGet moreGet = new HttpGet(more_info_url.toString());
+					DefaultHttpClient httpClient3 = new DefaultHttpClient();
+					moreGet.setHeader("Referer", "http://api1.t.qq.com/proxy.html");
+					moreGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:19.0) Gecko/20100101 Firefox/19.0");
+					httpClient3.setCookieStore(cookieStore);
+					response = httpClient3.execute(moreGet);
+					
+					context = EntityUtils.toString(response.getEntity());
+					
+					nextPgInfo = parserJSON(context);
+				} else {
+					return;
+				}
+			}
+			
+		} else {
+			System.out.print(userName+"$ ");
+			System.out.println(msg);
+		}
+	}
+	
+	// 解析微博JSON数据
+	public static String[] parserJSON(String context) {
+		JsonParser jsonParser = new JsonParser();
+		JsonElement jsonElement = jsonParser.parse(context);
+		JsonObject jsonObj = jsonElement.getAsJsonObject();
+		
+		JsonElement resultEmt = jsonObj.get("result");
+		int result = resultEmt.getAsInt();
+		
+		// 请求微博信息成功
+		if(result==0) {
+			JsonElement infoElm = jsonObj.get("info");
+			JsonObject infoObj = infoElm.getAsJsonObject();
+			
+			// 判断是否还有下一页
+			JsonElement hasNextElm = infoObj.get("hasNext");
+			int hasNext = hasNextElm.getAsInt();
+			
+			JsonArray talks = infoObj.getAsJsonArray("talk");
+			for(int i=0; i<talks.size(); i++) {
+				JsonElement weiboElm = talks.get(i);
+				JsonObject weiboObj = weiboElm.getAsJsonObject();
+				// 得到昵称
+				JsonElement nickElm = weiboObj.get("nick");
+				String nick = nickElm.getAsString();
+				// 得到备注名称
+				JsonElement bknameElm = weiboObj.get("bkname");
+				String bkname = bknameElm.getAsString();
+				
+				// 得到内容
+				JsonElement contentElm = weiboObj.get("content");
+				String news = contentElm.getAsString().replaceAll("<.*?>", "");
+				
+				StringBuffer sb = new StringBuffer(nick);
+				if(!StringUtil.isBlank(bkname)) {
+					sb.append("【").append(bkname).append("】");
+				}
+				sb.append(": ").append(news);
+				
+				// 判断是否存在转发内容
+				if(weiboObj.has("source")) {
+					sb.append("\r\n转自：\r\n");
+					
+					JsonObject sourceObj = weiboObj.getAsJsonObject("source");
+					// 得到昵称
+					JsonElement nickElmSrc = sourceObj.get("nick");
+					String nickSrc = nickElmSrc.getAsString();
+					// 得到备注名称
+					JsonElement bknameElmSrc = sourceObj.get("bkname");
+					String bknameSrc = bknameElmSrc.getAsString();
+					
+					// 得到内容
+					JsonElement contentElmSrc = sourceObj.get("content");
+					String newsSrc = contentElmSrc.getAsString().replaceAll("<.*?>", "");
+					
+					sb.append(nickSrc);
+					if(!StringUtil.isBlank(bknameSrc)) {
+						sb.append("【").append(bknameSrc).append("】");
+					}
+					sb.append(": ").append(newsSrc);
+				}
+				
+				System.out.println(sb.toString());
+				System.out.println("=========================================");
+				
+				// 得到最后一条微博和time和id，用于实现翻页
+				if(hasNext==1&&i==talks.size()-1) {
+					String[] nextPgInfo = new String[2];
+					// 得到time
+					JsonElement timeElem = weiboObj.get("timestamp");
+					nextPgInfo[0] = timeElem.getAsString();
+					// 得到ID
+					JsonElement idElem = weiboObj.get("id");
+					nextPgInfo[1] = idElem.getAsString();
+					
+					return nextPgInfo;
+				}
+			}
+		}
+		return null;
+	}
 	
 	public static void main(String[] args) throws URISyntaxException {
 		String accountNumber = "1070772010";
@@ -69,106 +315,20 @@ public class LoginTest {
 			
 			if(resultCode==0) {
 				// resultCode为0说明不需验证码
-				List<NameValuePair> formparams = new ArrayList<NameValuePair>();
-				formparams.add(new BasicNameValuePair("u", accountNumber));
-				formparams.add(new BasicNameValuePair("p", Encryptor.encrypt(uin, accountPassword, codeNum)));
-				formparams.add(new BasicNameValuePair("verifycode", codeNum));
-				formparams.add(new BasicNameValuePair("webqq_type", "10"));
-				formparams.add(new BasicNameValuePair("remember_uin","1"));
-				formparams.add(new BasicNameValuePair("login2qq", "1"));
-				formparams.add(new BasicNameValuePair("aid", "1003903"));
-				formparams.add(new BasicNameValuePair("u1", "http://web.qq.com/loginproxy.html?login2qq=1&webqq_type=10"));
-				formparams.add(new BasicNameValuePair("h", "1"));
-				formparams.add(new BasicNameValuePair("ptredirect", "0"));
-				formparams.add(new BasicNameValuePair("ptlang", "2052"));
-				formparams.add(new BasicNameValuePair("from_ui", "1"));
-				formparams.add(new BasicNameValuePair("pttype", "1"));
-				formparams.add(new BasicNameValuePair("dumy", ""));
-				formparams.add(new BasicNameValuePair("fp", "loginerroralert"));
-				formparams.add(new BasicNameValuePair("action", "2-13-47578"));
-				formparams.add(new BasicNameValuePair("mibao_css", "m_webqq"));
-				formparams.add(new BasicNameValuePair("t", "1"));
-				formparams.add(new BasicNameValuePair("g", "1"));
-				
-				String prams = URLEncodedUtils.format(formparams, "UTF-8");
-				StringBuffer login_url = new StringBuffer(USER_LOGIN_URL);
-				login_url.append("?").append(prams);
-				httpGet = new HttpGet(login_url.toString());
-				httpGet.setHeader("Referer", "http://qzone.qq.com/");
-				httpClient.execute(httpGet);
-				CookieStore cookieStore=((AbstractHttpClient) httpClient).getCookieStore();
-				
-				httpGet = new HttpGet(WEIBO_URL);
-				DefaultHttpClient httpClient2 = new DefaultHttpClient();
-				httpClient2.setCookieStore(cookieStore);
-				response = httpClient2.execute(httpGet);
-				cookieStore = httpClient2.getCookieStore();
-				
-				context = EntityUtils.toString(response.getEntity());
-				
-				Document document = Jsoup.parse(context);
-				Elements msgBoxs = document.select("#talkList li>div[class=msgBox]");
-				for(Element msgBox : msgBoxs) {
-					Elements userNames = msgBox.getElementsByClass("userName");
-					Elements msgCnts = msgBox.getElementsByClass("msgCnt");
-					if(userNames!=null&&userNames.size()>0&&msgCnts!=null&&msgCnts.size()>0) {
-						System.out.print(userNames.get(0).getElementsByTag("a").get(0).ownText());
-						System.out.print(": ");
-						System.out.println(msgCnts.get(0).ownText());
-					}
-					// 得到转发的内容
-					Elements replyBoxs = msgBox.getElementsByClass("replyBox");
-					if(replyBoxs!=null&&replyBoxs.size()>0) {
-						Elements replyBoxCnts = replyBoxs.get(0).getElementsByClass("msgCnt");
-						if(replyBoxCnts!=null&&replyBoxCnts.size()>0) {
-							Element replyBoxName = replyBoxCnts.get(0).getElementsByTag("a").get(0);
-							System.out.print(replyBoxName.ownText());
-							System.out.print(": ");
-							System.out.println(replyBoxCnts.get(0).ownText());
-						}
-					}
-					System.out.println("==================================");
-				}
-				
-				// 微博翻页
-				Elements lis = document.select("#talkList li:last-child");
-				Element last_info = lis.first();
-				
-				String id = last_info.attr("id");
-				String page = last_info.attr("from");
-				String time = last_info.attr("rel");
-				
-				List<NameValuePair> moreParams = new ArrayList<NameValuePair>();
-				moreParams.add(new BasicNameValuePair("time", time));
-				moreParams.add(new BasicNameValuePair("page", page));
-				moreParams.add(new BasicNameValuePair("id", id));
-				moreParams.add(new BasicNameValuePair("apiType", "8"));
-				moreParams.add(new BasicNameValuePair("apiHost", "http://api.t.qq.com"));
-				moreParams.add(new BasicNameValuePair("_r", String.valueOf(new Date().getTime())));
-				prams = URLEncodedUtils.format(moreParams, "UTF-8");
-				
-				//http://api1.t.qq.com/asyn/home.php?&&apiType=8&apiHost=http://api.t.qq.com&_r=1364199050480&_r=1364199050740
-				//http://api1.t.qq.com/asyn/home.php?&time=1364171103&page=4&id=234207009838751&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364191421224
-				//http://api1.t.qq.com/asyn/home.php?&time=1364176024&page=3&id=258252119653735&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364186420310
-				//http://api1.t.qq.com/asyn/home.php?&time=1364121424&page=9&id=235984052151296&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364191855494
-				
-				//http://api1.t.qq.com/asyn/home.php?&time=1364194623&page=1&id=251950051372155&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364198709007
-				//http://api1.t.qq.com/asyn/home.php?&time=1364194933&page=2&id=181885090731332&apiType=8&apiHost=http%3A%2F%2Fapi.t.qq.com&_r=1364198749991
-				StringBuffer more_info_url = new StringBuffer(MORE_INFO_URL);
-				more_info_url.append("?&").append(prams);
-				HttpGet moreGet = new HttpGet(more_info_url.toString());
-				System.out.println(more_info_url.toString());
-				DefaultHttpClient httpClient3 = new DefaultHttpClient();
-				moreGet.setHeader("rf", "http://t.qq.com/websoft1");
-				httpClient3.setCookieStore(cookieStore);
-				response = httpClient3.execute(moreGet);
-				
-				System.out.println(EntityUtils.toString(response.getEntity()));
+				login(httpClient, httpGet, uin, accountNumber, accountPassword, codeNum);
 			} else {
 				// resultCode为其他值，则验证密码
-				
+				System.out.println("需要输入验证码！");
+				JFrame frame = validationCode(httpClient, httpGet, accountNumber);
+				Scanner in = new Scanner(System.in);
+				System.out.print("请输入对话框中的验证码：");
+				String verifyCode = in.next();
+				if(frame!=null&&frame instanceof JFrame) {
+					frame.setVisible(false);
+					frame.dispose();
+				}
+				login(httpClient, httpGet,uin, accountNumber, accountPassword, verifyCode);
 			}
-			
 			
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
